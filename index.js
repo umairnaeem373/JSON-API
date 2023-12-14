af59590265989863a8f959833748d08905cfb468
umairnaeem373 <umairnaeem373@gmail.com>
2023-12-15 00:50:34 +0500

    json-api

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
-server.use(middlewares);
-server.use(router);
+server.use(middlewares)
+server.use(router)
 
-server.listen(port);
+server.listen(port)
